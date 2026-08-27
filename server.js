1af734c05f4e2f8b7368fbbbcda4edc17ae2a421
mining-usdt <bnmlghr@gmail.com>
2026-08-27 14:52:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1229,13 +1229,7 @@ app.post("/api/withdraw", async (req, res) => {
         // =========================================================
     // CHECK: WITHDRAWAL BLOCKED - CONTACT TELEGRAM
     // =========================================================
-   if (!user.withdrawalEnabled) {
-      return res.status(403).json({
-        success: false,
-        message: "⚠️ حدثت لدينا بعض المشكلات الفنية، لذا نرجو منك التواصل مع حسابنا الرسمي على تيليجرام لفتح طلب السحب الخاص بك ومتابعته.",
-        telegram: "https://t.me/miningusdtcontact"
-      });
-    }
+   
 
     // خصم الرصيد
     user.balance = currentBalance - amount;
